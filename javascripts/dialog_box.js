8f0ec342fdc26eb96c59d3348045692c418b8a9e
Brian Anderson <banders@banders-laptop.(none)>
2008-03-07 14:02:56 -0600

    added validation and error to those fields that aren't validated.

--- javascripts/dialog_box.js
+++ javascripts/dialog_box.js
@@ -5,26 +5,28 @@ DialogBox.prototype = {
 		this.element = $(el);
 		this.is_processing = false;
 		
+		this.required_fields = new Array();
+		this.errors = new Hash();
 		
 		this.closeObserver = this._closeWindow.bind(this);
+		this.findAllFields();
 		this.findRequiredFields();
 		this.setObserversOnFields();
-		
 		this.openWindow()
 	},
 	
 	save: function() {
-		if(Math.random() > 0.5) {
-				this.notValid();
-			} else {
-				this.closeWindow();
-		}
+	  if(this.checkIfValid()) {
+	    this.closeWindow();
+	  } else {
+	    console.log("Not valid! " + this.errors.inspect());
+	    this.notValid();
+	  }
 	},
 	
 	cancel: function() {
 		new Effect.Fade(this.element);
 	},
-	
 	
 	openWindow: function(){
 		new Effect.Appear(this.element);
@@ -44,6 +46,11 @@ DialogBox.prototype = {
 	},
 
 	notValid:function(){
+	  // Remove all the not_valid classes
+	  this.all_the_fields.each(function(el){ $(el).removeClassName('not_valid'); });
+	
+	  // Add 'not_valid' classes to those fields that are not valid
+	  this.errors.keys().each(function(field){$(field).addClassName('not_valid'); });
 		new Effect.Shake(this.element);
 	},
 	
@@ -58,10 +65,14 @@ DialogBox.prototype = {
     });
 	},
 	
-	setObserversOnFields: function(){
+	findAllFields: function(){
+	  this.all_the_fields = $(this.element).select("input","textarea","select");
+	},
+	
+	setObserversOnFields: function(){	  
 		$$('.dialog_box fieldset input','.dialog_box fieldset textarea').each(function(el){
-      Event.observe(el,"focus", function(){ this.focusField(el); });
-      Event.observe(el,"select",function(){ this.focusField(el); });
+      Event.observe(el,"focus", function(){ this.focusField(el);}.bind(this));
+      Event.observe(el,"select",function(){ this.focusField(el);}.bind(this));
 
       Event.observe(el,'focus', function(){
         if(el.hasClassName('sample')){
@@ -69,15 +80,33 @@ DialogBox.prototype = {
           $(el).value="";
         };
       });
-    });
+    }.bind(this));
 	},
 
 	findRequiredFields: function(){
 	  this.element.select(".required").each(function(el){
-      // alert("Required field: " + el.id);
-      console.log('Required fields are: ' + el.id);
-	  });
+	    this.required_fields.push(el.id);
+	    this.denoteRequiredField(el.id);
+	  }.bind(this));
 	},
+  
+
+  denoteRequiredField: function(field_id){
+    var label = $(field_id).previous('label');
+    label.addClassName('required');
+    label.update(label.innerHTML + ' *'); 
+    $(field_id).addClassName('required');
+  },
+
+  checkIfValid: function(){
+    this.errors = new Hash();
+    this.required_fields.each(function(field){
+      if($(field).hasClassName('sample') || $(field).value.length < 1){
+        this.errors.set(field, "must be filled in");
+      } 
+    }.bind(this));
+    return (this.required_fields.keys().length == 0);
+  },
 
 	_closeWindow: function(){
 		new Effect.SwitchOff(this.element);
